9ed91eab3d1890a10ef6ff26acc99767e6961764
OlaFro <olka.be@gmail.com>
2021-03-15 14:20:46 +0100

    content in search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import { StyledSearch } from "../styled-components/StyledSearch";
 export default function Search() {
   const [data, setData] = useState("");
   const [cities, setCities] = useState([]);
@@ -38,13 +39,35 @@ export default function Search() {
   }
 
   return (
-    <div>
-      <select>
-        Choose city
-        <option>-------</option>
-        {cities ? showOptions() : null}
-      </select>
+    <StyledSearch>
+      <div>
+        <label id="region">Region</label>
+        <select name="region" id="region">
+          <option>Choose city</option>
+          {cities ? showOptions() : null}
+        </select>
+      </div>
+      <div>
+        <label id="start">Day of Arrival</label>
+        <input type="date" id="start" name="start" />
+      </div>
+      <div>
+        <label id="end">Day of Departure</label>
+        <input type="date" id="end" name="end" />
+      </div>
+      <div>
+        <label id="rating">Rating</label>
+        <select name="rating" id="rating">
+          <option>Choose rating</option>
+          <option value="5">★★★★★</option>
+          <option value="4">★★★★☆</option>
+          <option value="3">★★★☆☆</option>
+          <option value="2">★★☆☆☆</option>
+          <option value="1">★☆☆☆☆</option>
+        </select>
+      </div>
+
       <button onClick={handleClick}> Search</button>
-    </div>
+    </StyledSearch>
   );
 }
---
+++ src/styled-components/StyledSearch.jsx
@@ -0,0 +1,14 @@
+import styled, { css } from "styled-components";
+
+export const StyledSearch = styled.section`
+  width: 80%;
+  max-width: 800px;
+  margin: 15vh auto 5vh auto;
+  background-color: #d5cabd;
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+  > * {
+    margin: 20px;
+  }
+`;
